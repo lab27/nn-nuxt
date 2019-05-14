915d323affd18387c5de6e244d499ab5d099db0a
Nick Niles <nickyniles@gmail.com>
2019-05-14 22:24:00 +0200

    [init] begin

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  div
+    Logo
+    nuxt
+</template>
+
+<script>
+import Logo from '~/components/NNLogo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div
+    section.jumbotron
+      h1.title.has-text-centered I'm not young. But I know stuff. 
+      
+</template>
+
+<script>
+import Logo from '~/components/NNLogo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss">
+.jumbotron {
+  padding: 10vw;
+
+  h1 {
+  font-size: 10vw;
+  font-weight: 700;
+  }
+}
+</style>
